02692ded82e53a4051146ddebc0d5c5fce30b13c
FadyGamilM <gamilfady605@gmail.com>
2024-11-29 20:49:36 +0200

    publish to the topic

--- chat.go
+++ chat.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/ably/ably-go/ably"
 	"github.com/joho/godotenv"
@@ -18,4 +21,25 @@ func main() {
 		ably.WithEchoMessages(false),
 	)
 	log.Println(client.Auth.ClientID() + " joined")
+
+	// then we define the topic (the channel)
+	chatTopic := client.Channels.Get("chat")
+
+	// define a reader to read from the stdin
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println("error_reading_the_msg.", err.Error())
+			continue
+		}
+
+		msg = strings.ReplaceAll(msg, "\n", "")
+
+		if err = chatTopic.Publish(context.Background(), "message", msg); err != nil {
+			log.Println("error_publishing_msg.", err.Error())
+			continue
+		}
+	}
 }

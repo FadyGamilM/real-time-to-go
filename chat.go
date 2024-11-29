e1216c9542146cc0637dc1a7661425fcf73e5ebf
FadyGamilM <gamilfady605@gmail.com>
2024-11-29 22:54:00 +0200

    handle presence to track joins and leaves

--- chat.go
+++ chat.go
@@ -5,7 +5,9 @@ import (
 	"context"
 	"log"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 
 	"github.com/ably/ably-go/ably"
 	"github.com/joho/godotenv"
@@ -24,6 +26,39 @@ func main() {
 
 	// then we define the topic (the channel)
 	chatTopic := client.Channels.Get("chat")
+
+	// SUB
+	chatTopic.SubscribeAll(
+		context.Background(),
+		func(m *ably.Message) {
+			log.Printf("received_msg_from_%s : %s\n", m.ClientID, m.Data)
+		},
+	)
+
+	// Check for presence
+	// first we enter the set of presence
+	chatTopic.Presence.Enter(context.Background(), "")
+	// we listen for any leave of joining
+	chatTopic.Presence.SubscribeAll(
+		context.Background(),
+		func(pm *ably.PresenceMessage) {
+			if pm.Action == ably.PresenceActionEnter {
+				log.Printf("%s_has_joined\n", pm.ClientID)
+			} else if pm.Action == ably.PresenceActionLeave {
+				log.Printf("%s_has_left\n", pm.ClientID)
+			}
+		},
+	)
+	// if we terminted the program we need to exit the presence set
+	exitPresenceChan := make(chan os.Signal, 1)
+	signal.Notify(exitPresenceChan, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		// block until we hear a closing signal
+		<-exitPresenceChan
+		// exit
+		chatTopic.Presence.Leave(context.Background(), "")
+		os.Exit(0)
+	}()
 
 	// define a reader to read from the stdin
 	reader := bufio.NewReader(os.Stdin)
